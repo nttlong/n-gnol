6e72eacc09249346e67867227db58132f1a7715a
nttlong <zugeliang2000@gamil.com>
2018-12-23 21:54:52 +0700

    save

--- controller-wrapper.js
+++ controller-wrapper.js
@@ -1,68 +1,68 @@
-var method=require("./action-wrapper");
+var method = require("./action-wrapper");
 var filewatcher = require('filewatcher');
 var ControllerActionSender = require("./controller-action-sender");
-var controllerView =require("./view");
+var controllerView = require("./view");
 var watcher = filewatcher();
-class ControllerWrapper{
+class ControllerWrapper {
     /**
      * 
      * @param {*} config 
      * @param {*} app 
      */
-    constructor(config,app){
-        
-        if(config.url===undefined){
-            throw("'url' was not found");
+    constructor(config, app) {
+
+        if (config.url === undefined) {
+            throw ("'url' was not found");
         }
-        if (config.template===undefined){
+        if (config.template === undefined) {
             throw ("'template' was not found");
         }
-        if (config.fileName===undefined) {
-            throw ( new Error("'fileName' was not found"));
+        if (config.fileName === undefined) {
+            throw (new Error("'fileName' was not found"));
         }
-        if (config.app===undefined) {
-            throw ( new Error("'app' object was not found"));
+        if (config.app === undefined) {
+            throw (new Error("'app' object was not found"));
         }
-        if (config.app.hostDir===undefined) {
+        if (config.app.hostDir === undefined) {
             throw (new Error("'app.hostDir' object was not found"));
         }
-        if (config.app.fullPath===undefined) {
+        if (config.app.fullPath === undefined) {
             throw (new Error("'app.fullPath' object was not found"));
         }
-        if (config.app.name===undefined) {
+        if (config.app.name === undefined) {
             throw (new Error("'app.name' was noy found"));
         }
-        if(config.settings===undefined){
-            throw(new Error('settings was not found'));
+        if (config.settings === undefined) {
+            throw (new Error('settings was not found'));
         }
-        if(config.settings.app===undefined){
-            throw(new Error("'settings.app' was not found"));
+        if (config.settings.app === undefined) {
+            throw (new Error("'settings.app' was not found"));
         }
-        this.settings=config.settings;
-        this.url=config.url;
-        this.template=config.template;
-        this.fileName=config.fileName;
-        this.app={
-            
+        this.settings = config.settings;
+        this.url = config.url;
+        this.template = config.template;
+        this.fileName = config.fileName;
+        this.app = {
+
         };
-        Object.keys(config.app).forEach(k=>{
-            this.app[k]=config.app[k];
+        Object.keys(config.app).forEach(k => {
+            this.app[k] = config.app[k];
         })
-        this.controllersCache=config.controllersCache;
-        this.controller=this.controllersCache[this.fileName];
+        this.controllersCache = config.controllersCache;
+        this.controller = this.controllersCache[this.fileName];
         this.apply();
         watcher.add(this.fileName);
-        var me=this;
-        watcher.on('change', (file,stat)=>{
+        var me = this;
+        watcher.on('change', (file, stat) => {
             delete require.cache[file];
             me.controller = require(file);
             console.log('File modified: %s', file);
             if (!stat) console.log('deleted');
         });
-        
+
     }
-    execMethod(methodOfActions,req,res,next){
-        if (methodOfActions.privilege===undefined) {
+    execMethod(methodOfActions, req, res, next) {
+        if (methodOfActions.privilege === undefined) {
             throw (new Error(`The privilege  of action 'get' in ${this.fileName} was not found`))
         }
         var isAllow = methodOfActions.privilege === "public";
@@ -77,19 +77,19 @@ class ControllerWrapper{
             } else {
                 onAuthorize = this.app.onAuthorize;
             }
-            isAllow = onAuthorize(this, req, res,next);
+            isAllow = onAuthorize(this, req, res, next);
         }
         if (isAllow) {
             var sender = new ControllerActionSender();
-            sender.app=this.app;
-            sender.request=req;
-            sender.response=res;
-            sender.settings=this.settings;
-            sender.template=this.template;
-            sender.url=this.url;
-            sender.next=next;
-            var ret= methodOfActions.method(sender);
-            if (ret instanceof controllerView){
+            sender.app = this.app;
+            sender.request = req;
+            sender.response = res;
+            sender.settings = this.settings;
+            sender.template = this.template;
+            sender.url = this.url;
+            sender.next = next;
+            var ret = methodOfActions.method(sender);
+            if (ret instanceof controllerView) {
                 ret.render()
             }
             return ret;
@@ -98,15 +98,15 @@ class ControllerWrapper{
 
         }
     }
-    apply(){
-        if(this.controller.actions===undefined){
-            this.controller.actions.get=(sender,req,res)=>{
+    apply() {
+        if (this.controller.actions === undefined) {
+            this.controller.actions.get = (sender, req, res) => {
 
             }
         }
-        var me=this;
-        if(this.app.hostDir){
-            if(this.settings.hostDir===null){
+        var me = this;
+        if (this.app.hostDir && this.app.hostDir !== null) {
+            if (this.settings.hostDir === null) {
                 this.settings.app.get("/" + this.app.hostDir + "/" + this.url, (req, res, next) => {
                     me.execMethod(me.controller.actions.get, req, res, next);
 
@@ -118,21 +118,21 @@ class ControllerWrapper{
                 }
             }
             else {
-                this.settings.app.get("/" +this.settings.hostDir+"/" + this.app.hostDir + "/" + this.url, (req, res, next) => {
+                this.settings.app.get("/" + this.settings.hostDir + "/" + this.app.hostDir + "/" + this.url, (req, res, next) => {
                     me.execMethod(me.controller.actions.get, req, res, next);
 
                 });
                 if (me.controller.actions.post !== undefined) {
-                    this.settings.app.post("/" + this.settings.hostDir +"/" + this.app.hostDir + "/" + this.url, (req, res, next) => {
+                    this.settings.app.post("/" + this.settings.hostDir + "/" + this.app.hostDir + "/" + this.url, (req, res, next) => {
                         me.execMethod(me.controller.actions.post, req, res, next);
                     });
                 }
             }
-            
+
         }
-        else{
-            if (this.settings.hostDir === undefined) {
-                this.settings.app.get("/"+this.url, (req, res) => {
+        else {
+            if (this.settings.hostDir === null) {
+                this.settings.app.get("/" + this.url, (req, res) => {
                     me.execMethod(me.controller.actions.get, req, res, next);
                 });
                 if (me.controller.actions.post !== undefined) {
@@ -142,17 +142,17 @@ class ControllerWrapper{
                 }
             }
             else {
-                this.settings.app.get("/" + this.settings.hostDir +"/" + this.url, (req, res) => {
+                this.settings.app.get("/" + this.settings.hostDir + "/" + this.url, (req, res) => {
                     me.execMethod(me.controller.actions.get, req, res, next);
                 });
                 if (me.controller.actions.post !== undefined) {
-                    this.settings.app.post("/" + this.settings.hostDir +"/" + this.url, (req, res, next) => {
+                    this.settings.app.post("/" + this.settings.hostDir + "/" + this.url, (req, res, next) => {
                         me.execMethod(me.controller.actions.post, req, res, next);
                     });
                 }
             }
         }
-       
+
 
 
     }
--- load-apps.js
+++ load-apps.js
@@ -1,28 +1,28 @@
-var controllersCache={};
-var controllerInstance={}
-var ControllerWrapper=require("./controller-wrapper");
-module.exports =function(){
+var controllersCache = {};
+var controllerInstance = {}
+var ControllerWrapper = require("./controller-wrapper");
+module.exports = function () {
     const express = require('express');
     const epApp = express();
-    var Info=require("./app-process-info").Info;
-    var settings=require("./settings");
-    
-    var fs=require("fs");
-    var path=require("path");
-    var appsDir=path.sep.join(settings.workingDir,"apps");
-    var dirs=fs.readdirSync(appsDir);
-    for(var i=0;i<dirs.length;i++){
+    var Info = require("./app-process-info").Info;
+    var settings = require("./settings");
+
+    var fs = require("fs");
+    var path = require("path");
+    var appsDir = path.sep.join(settings.workingDir, "apps");
+    var dirs = fs.readdirSync(appsDir);
+    for (var i = 0; i < dirs.length; i++) {
         var appDir = path.sep.join(appsDir, dirs[i]);
-        var appSettingPath=path.sep.join(appDir,"settings.js");
-        var appSettings={
+        var appSettingPath = path.sep.join(appDir, "settings.js");
+        var appSettings = {
             hostDir: dirs[i],
-            appDir:appDir,
-            staticDir:"static"
+            appDir: appDir,
+            staticDir: "static"
         };
-        var appName=dirs[i];
+        var appName = dirs[i];
         var appSets = require(appSettingPath);
-        if(appSets.hostDir===undefined){
-            throw(new Error(`"hostDir" was not found in ${appSettingPath}`));
+        if (appSets.hostDir === undefined) {
+            throw (new Error(`"hostDir" was not found in ${appSettingPath}`));
         }
         if (appSets.staticDir === undefined) {
             throw (new Error(`"staticDir" was not found in ${appSettingPath}`));
@@ -36,85 +36,91 @@ module.exports =function(){
         if (appSets.middleWare === undefined) {
             throw (new Error(`"middleWare" was not found in ${appSettingPath}`));
         }
-        
+
         Object.keys(appSets).forEach(key => {
             appSettings[key] = appSets[key];
         });
-        if (appSettings.staticDir == null) {
+        if (appSettings.staticDir === null) {
             appSettings.staticDir = "static";
-        } 
+        }
+        if (appSettings.hostDir == null) {
+            appSettings.hostDir = dirs[i];
+        }
+        else if (appSettings.hostDir === "") {
+            appSettings.hostDir = undefined;
+        }
         var AppStaticDir = path.sep.join(appDir, appSettings.staticDir);
-        if(settings.hostDir!=null){
-            if (appSettings.hostDir!=null) {
-                epApp.use("/" + settings.hostDir+"/" + appSettings.hostDir + '/static', express.static(AppStaticDir));
+        if (settings.hostDir != null) {
+            if (appSettings.hostDir != null) {
+                epApp.use("/" + settings.hostDir + "/" + appSettings.hostDir + '/static', express.static(AppStaticDir));
             }
             else {
-                epApp.use("/" + settings.hostDir +'/static', express.static(AppStaticDir));
+                epApp.use("/" + settings.hostDir + '/static', express.static(AppStaticDir));
             }
         }
         else {
-            if (appSettings.hostDir!=null) {
+            if (appSettings.hostDir != null) {
                 epApp.use("/" + appSettings.hostDir + '/static', express.static(AppStaticDir));
             }
             else {
                 epApp.use('/static', express.static(AppStaticDir));
             }
         }
-        
-      
+
+
         epApp.use("/" + appSettings.hostDir + '/static', express.static(AppStaticDir));
         var stat = fs.statSync(appDir);
-        if (stat.isDirectory()){
-            var controllerDir = path.sep.join(appDir,"controllers");
-            var controllerDirSubDirs=fs.readdirSync(controllerDir);
-            for(var j=0;j<controllerDirSubDirs.length;j++){
+        if (stat.isDirectory()) {
+            var controllerDir = path.sep.join(appDir, "controllers");
+            var controllerDirSubDirs = fs.readdirSync(controllerDir);
+            for (var j = 0; j < controllerDirSubDirs.length; j++) {
                 var controllerFile = path.join(controllerDir, controllerDirSubDirs[j]);
-                
+
                 controllersCache[controllerFile] = require(controllerFile);
                 var config = controllersCache[controllerFile];
-                if (appSettings.hostDir===undefined){
+                if (appSettings.hostDir === undefined) {
                     throw (new Error(`"hostDir" was not found in "${appDir}.settings.js"`))
                 }
-                var controllerConfig={
-                    fileName:controllerFile,
-                    url:config.url,
-                    template:config.template,
-                    app:{
+                var controllerConfig = {
+                    fileName: controllerFile,
+                    url: config.url,
+                    template: config.template,
+                    app: {
                         hostDir: (appSettings.hostDir == null) ? undefined : appSettings.hostDir,
-                        fullPath:appSettings.appDir,
-                        name:appName,
+                        fullPath: appSettings.appDir,
+                        name: appName,
                         template: (appSettings.viewsDir == null) ? "views" : appSettings.viewsDir,
-                        authenticate:appSettings.authenticate,
-                        middleWare:appSettings.middleWare
+                        authenticate: appSettings.authenticate,
+                        middleWare: appSettings.middleWare
                     },
                     controllersCache: controllersCache,
-                    settings:{
-                        app:epApp
+                    settings: {
+                        app: epApp
                     }
 
                 }
-                Object.keys(settings).forEach(k=>{
-                    controllerConfig.settings[k]=settings[k];
+                Object.keys(settings).forEach(k => {
+                    controllerConfig.settings[k] = settings[k];
                 })
-                Object.keys(appSettings).forEach(k=>{
-                    controllerConfig.app[k]=appSettings[k];
+                Object.keys(appSettings).forEach(k => {
+                    controllerConfig.app[k] = appSettings[k];
                 })
                 // var sender={
                 //     app:appSettings,
                 //     settings:settings
                 // }
-                controllerInstance[controllerFile] = new ControllerWrapper(controllerConfig,epApp);
-                
-                
-                
+                controllerInstance[controllerFile] = new ControllerWrapper(controllerConfig, epApp);
+
+
+
 
             }
         }
-            
-        
+
+
     }
-    
-   
+
+
 
     console.log(settings.workingDir);
     return epApp;
